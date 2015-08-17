1d27bb9504b1a10ebed367c6337f3c288091dbe3
viktor_novak <viktor_novak>
2015-08-17 19:46:31 +0300

    added on click to GET on user by id

--- app/js/app.js
+++ app/js/app.js
@@ -5,14 +5,24 @@ function httpGet(theUrl) {
   return xmlHttp.responseText;
 }
 
+function getUser(id) {
+  return httpGet('http://localhost:3000/users/' + id)
+}
+
 var users = httpGet('http://localhost:3000/users'),
     userDiv = document.getElementById('users'),
     ul = document.createElement('ul');
 users = JSON.parse(users);
 
 for (var i = users.length - 1; i >= 0; i--) {
-  var li = document.createElement('li');
-  li.textContent = users[i].name;
-  ul.appendChild(li);
+  (function() {
+    var li = document.createElement('li');
+    li.textContent = users[i].name;
+    var id = users[i].id;
+    li.addEventListener('click', function() {alert(getUser(id));});
+    ul.appendChild(li);
+  })(users[i])
 };
 userDiv.appendChild(ul);
+
+
