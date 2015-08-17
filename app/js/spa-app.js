5ff054dd2107c5cd29c859ee4d54d35a014b347e
AndrewLolo <andrewloloal@gmail.com>
2015-08-18 01:25:12 +0300

    Added spa-app.js skeleton

---
+++ app/js/spa-app.js
@@ -0,0 +1,15 @@
+(function(window){
+
+    var pageHandlers = {};
+    var currentPage;
+  
+    function show(){};
+    function app(){};
+    
+    app.handler = function(handler){};
+    
+    function onhashchange(){};
+    
+    window.app = app;
+
+})(this);
