3aa09729a3545b5b04ab09e03cd18d38c60a94f5
viktor_novak <viktor_novak>
2015-08-18 21:35:53 +0300

    added update request

--- app/js/app.js
+++ app/js/app.js
@@ -1,3 +1,7 @@
+/**
+ * REST
+ */
+
 function httpGet(theUrl) {
   var xmlHttp = new XMLHttpRequest();
   xmlHttp.open( "GET", theUrl, false );
@@ -10,9 +14,9 @@ function httpPost(theUrl, data) {
   xmlHttp.open( "POST", theUrl, false );
   xmlHttp.setRequestHeader('Content-Type', 'application/json');
   xmlHttp.onreadystatechange = function () {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-          alert(xmlHttp.responseText);
-      }
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      console.log(xmlHttp.responseText);
+    }
   }
   xmlHttp.send( JSON.stringify( data ) );
 }
@@ -22,9 +26,9 @@ function httpDel(theUrl, id) {
   xmlHttp.open( "DELETE", theUrl + '/' + id, false);
   xmlHttp.setRequestHeader('Content-Type', 'application/json');
   xmlHttp.onreadystatechange = function () {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-          alert(xmlHttp.responseText);
-      }
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      console.log(xmlHttp.responseText);
+    }
   }
   xmlHttp.send( null )
 }
@@ -34,11 +38,19 @@ function httpPut(theUrl, id, data) {
   xmlHttp.open( "PUT", theUrl + '/' + id, false);
   xmlHttp.setRequestHeader('Content-Type', 'application/json');
   xmlHttp.onreadystatechange = function () {
-      if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
-          alert(xmlHttp.responseText);
-      }
+    if (xmlHttp.readyState == 4 && xmlHttp.status == 200) {
+      console.log(xmlHttp.responseText);
+    }
   }
-  xmlHttp.send( data )
+  xmlHttp.send( JSON.stringify( data ) )
+}
+
+/**
+ * User REST
+ */
+
+function getUsers() {
+  return httpGet('http://localhost:3000/users')
 }
 
 function getUser(id) {
@@ -53,11 +65,27 @@ function delUser(id) {
   return httpDel('http://localhost:3000/users', id)
 }
 
-function editUser(id, data) {
-  return httpDel('http://localhost:3000/users/' + id, data)
+function updateUser(data) {
+  return httpPut('http://localhost:3000/users', data.id, data)
 }
 
-var users = httpGet('http://localhost:3000/users'),
+/**
+ * Helpers temporary
+ */
+
+function loadUserToForm (id) {
+  var editUserForm = document.getElementById('editUser'),
+      user = JSON.parse(getUser(id));
+  editUserForm.elements.id.value = user.id;
+  editUserForm.name.value = user.name;
+  editUserForm.email.value = user.email;
+}
+
+/*
+Load all users
+ */
+
+var users = getUsers(),
     userDiv = document.getElementById('users'),
     ul = document.createElement('ul');
 users = JSON.parse(users);
@@ -66,13 +94,17 @@ for (var i = users.length - 1; i >= 0; i--) {
   (function() {
     var li = document.createElement('li'),
         id = users[i].id,
-        delBtn = document.createElement('input');
+        delBtn = document.createElement('input'),
+        editBtn;
     li.textContent = users[i].name;
     delBtn.type = 'button';
+    editBtn = delBtn.cloneNode(true);
     delBtn.value = 'Del';
-    delBtn.addEventListener('click', function() {return delUser(id)});
-
+    editBtn.value = 'Edit';
+    delBtn.addEventListener('click', function() {delUser(id); location.reload();});
+    editBtn.addEventListener('click', function() {loadUserToForm(id)});
     li.appendChild(delBtn);
+    li.appendChild(editBtn);
     ul.appendChild(li);
   })(users[i])
 };
@@ -93,6 +125,27 @@ function newUser(e) {
     };
     this.name.value = '';
     this.email.value = '';
+  location.reload();
+  return false;
+}
+
+/**
+ * Form handler for new Edit form
+ */
+
+function editUser(e) {
+  if (e.preventDefault) e.preventDefault();
+    if (this.name.value && this.email.value) {
+      var editUser = JSON.parse(getUser(this.id.value));
+      editUser.name = this.name.value;
+      editUser.email = this.email.value;
+      console.log(editUser);
+      updateUser(editUser);
+    };
+    this.id.value = '';
+    this.name.value = '';
+    this.email.value = '';
+  location.reload();
   return false;
 }
 
@@ -103,24 +156,6 @@ if (newUserForm.attachEvent) {
     newUserForm.attachEvent("submit", newUser);
 } else {
     newUserForm.addEventListener("submit", newUser);
-}
-
-/**
- * Form handler for new Edit form
- */
-
-function editUser(e) {
-  if (e.preventDefault) e.preventDefault();
-    if (this.name.value && this.email.value) {
-      var editUser = {};
-      editUser.name = this.name.value;
-      editUser.email = this.email.value;
-      console.log(editUser);
-      postUser(editUser);
-    };
-    this.name.value = '';
-    this.email.value = '';
-  return false;
 }
 
 // Catch form for update of existing user
