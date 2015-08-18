8ba8aa5dedf651eced2460f2e67ab520e3df3e7c
AndrewLolo <andrewloloal@gmail.com>
2015-08-19 01:41:19 +0300

    Added SPA functionality

--- app/js/spa-app.js
+++ app/js/spa-app.js
@@ -1,15 +1,36 @@
-(function(window){
-
-    var pageHandlers = {};
-    var currentPage;
+(function(window) {  
   
-    function show(){};
-    function app(){};
+    function show(pageName){
+      var page = document.getElementById(pageName);
+      document.querySelector("nav li.active").classList.remove("active");
+      document.querySelector("nav li a[href="+"'#"+pageName+"'"+"]").closest("li").classList.add("active");
+      
+      document.body.setAttribute("page", pageName);
+      
+      document.querySelector("section.active").classList.remove("active");
+      document.getElementById(pageName).classList.add("active");
+    };
+  
+    function load(pageName){
+      var page = document.getElementById(pageName);
+      var src = page.getAttribute("src");
+      
+      var xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function() {
+        if (xhttp.readyState == 4 && xhttp.status == 200) {
+          page.innerHTML=xhttp.responseText;
+          show(pageName);
+        }
+      };
+      xhttp.open('GET', src, true);
+      xhttp.send();
+    };
+  
+    function onhashchange(){
+      var hash = location.hash || "#intro";
+      load(hash.substring(1));
+    };
     
-    app.handler = function(handler){};
-    
-    function onhashchange(){};
-    
-    window.app = app;
+    window.addEventListener("hashchange", onhashchange, false);
 
 })(this);
